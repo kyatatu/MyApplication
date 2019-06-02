5aa32077e756fe75e3a0e761ef98423281b97b0f
kyatatu <kyatatu912@icloud.com>
2019-06-02 12:41:48 +0900

    add: TextView, Button追加

--- app/src/main/kotlin/com/example/myapplication/MainActivity.kt
+++ app/src/main/kotlin/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeTextView(view: View){
+        messageTextView.text = "Hello there! nasu!"
     }
 }
